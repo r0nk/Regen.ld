9e4474e4dabec1782cc4acb7519350e147233e4f
ajohn268 <82244678+ajohn268@users.noreply.github.com>
2022-01-29 00:59:55 -0600

    Create database.sql
    stuff to create the database

---
+++ database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Users (
+    User_ID   STRING PRIMARY KEY,
+    User_Name STRING
+);
+
+CREATE TABLE Tasks (
+    Task_ID       INTEGER  PRIMARY KEY,
+    Task_Name     STRING   NOT NULL,
+    Task_Due_Date DATETIME,
+    Task_Est_Min  INTEGER
+);
+
+CREATE TABLE User_Tasks (
+    User_ID           STRING  REFERENCES Users (User_ID),
+    Task_ID           INT     REFERENCES Tasks (Task_ID),
+    Task_Is_Completed BOOLEAN DEFAULT (FALSE) 
+);
