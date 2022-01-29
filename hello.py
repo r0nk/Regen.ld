00ad2438762c9d6ef69af9348a5174345a53a7ca
r0nk <r0nk@r0nk.com>
2022-01-29 02:48:24 +0000

    it says hello now.

--- hello.py
+++ hello.py
@@ -1,3 +1,22 @@
 #!/bin/python
+import discord
+from discord.ext import commands
 
-print("Hello world!")
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content[0] == '*':
+            print(message.author,":",message.content)
+            if message.content.lower().startswith("*hello"):
+                await message.channel.send("hello world!")
+
+client = MyClient()
+client.run((open('token.txt','r').read().splitlines()[0]));
+con.close()
+
