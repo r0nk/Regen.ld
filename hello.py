b32db6df05b0c50bc822abf19e0b633bcb428ffc
r0nk <r0nk@r0nk.com>
2022-01-29 03:31:22 +0000

    task placeholders.

--- hello.py
+++ hello.py
@@ -11,10 +11,16 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content[0] == '*':
+        if message.content[0] == '/':
             print(message.author,":",message.content)
-            if message.content.lower().startswith("*hello"):
+            if message.content.lower().startswith("/hello"):
                 await message.channel.send("hello world!")
+            if message.content.lower().startswith("/add_task"):
+                await message.channel.send("add task here")
+            if message.content.lower().startswith("/show_tasks"):
+                await message.channel.send("This is a list of tasks")
+            if message.content.lower().startswith("/finish_task"):
+                await message.channel.send("mark a task as completed for a user")
 
 client = MyClient()
 client.run((open('token.txt','r').read().splitlines()[0]));
