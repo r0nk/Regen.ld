f3d2a865b373aef5de0db3390babce1fa8fa5575
daviddunnjr <98627534+daviddunnjr@users.noreply.github.com>
2022-01-29 14:06:31 -0600

    Added shorthand command options

--- hello.py
+++ hello.py
@@ -5,12 +5,11 @@ import sqlite3
 import time
 import maya
 from datetime import datetime
-
+from dateutil.relativedelta import relativedelta
 
 con = sqlite3.connect('regen.db')
 cur = con.cursor()
 def initalize_databases():
-
     cur.execute("CREATE TABLE IF NOT EXISTS Users ( User_ID   STRING PRIMARY KEY, User_Name STRING);")
     con.commit()
 
@@ -19,7 +18,9 @@ def initalize_databases():
 
     cur.execute("CREATE TABLE IF NOT EXISTS User_Tasks ( User_ID           STRING  REFERENCES Users (User_ID), Task_ID           INT     REFERENCES Tasks (Task_ID), Task_Is_Completed BOOLEAN DEFAULT (FALSE) ); ")
     con.commit()
-    #TODO schedule table
+
+    cur.execute("CREATE TABLE IF NOT EXISTS Scheduler (  User_ID   STRING PRIMARY KEY REFERENCES User_Tasks (User_ID),     Task_List STRING REFERENCES Tasks (Task_Name),    Date      DATE);  ")
+    con.commit()
 
 initalize_databases()
 
@@ -33,14 +34,19 @@ class Task:
   def set_task(self, name):
     self.name = name;
 
+def task_urgency(t):
+    n = datetime.now()
+    rd = relative_delta(n,t.due)
+    hours_till_deadline=(rd.days*24+rd.hours)
+    return 1 - (hours_till_deadline + (t.time_est/10))
+
 #https://stackoverflow.com/questions/16891340/remove-a-prefix-from-a-string
 def remove_prefix(text, prefix):
     if text.startswith(prefix):
         return text[len(prefix):]
     return text  # or whatever
 
-def add_task(msg):
-    ret = ""
+def read_task_from_message(msg):
     t = Task("placeholder_tn",datetime.now(),60)
     for i in msg.split()[1:]:
         if "@" in i:
@@ -54,6 +60,11 @@ def add_task(msg):
             t.time_est=int(s)
         else:
             t.name=i
+    return t
+
+def add_task(msg):
+    ret = ""
+    t = read_task_from_message(msg) 
     if t.name == "placeholder_tn":
         return "No name found, task not added."
     cur.execute("insert into Tasks (Task_Name,Task_Due_Date,Task_Est_Min) values ( ?,datetime(),?)",(t.name,t.time_est))
@@ -67,26 +78,38 @@ def finish_task(msg):
         cur.execute("DELETE FROM Tasks WHERE Task_Name = '?';",(i,))
         con.commit()
 
+def edit_task(msg):
+    t = read_task_from_message(msg)
+    cur.execute("update Tasks set Task_Due_Date=(?) where Task_Name = (?)",(t.due,t.name))
+    cur.execute("update Tasks set Task_Est_Min=(?) where Task_Name = (?)",(t.time_est,t.name))
+    return "Task "+t.name+" updated!"
+
 def show_tasks(msg):
     #TODO this should only show tasks that are assigned to the user calling
     cur.execute("select * from Tasks")
     ret=""
     for name in cur.fetchall():
-        ret = ret + "* " + name[1] + "\n"
+        ret = ret + "* " + name[1] + "\t"+name[2]+"\t"+str(name[3])+"m\n"
     return ret
 
 #for each possible command, send it to its resepective function and 
 # send the user back its output
 async def handle_message(message):
-    if message.content.lower().startswith("/add_task"):
+    #Docs https://discordpy.readthedocs.io/en/stable/api.html?highlight=message#discord.Message
+    #TODO message.mentions targets a list of users
+    #TODO role_mentions gets a list of roles
+    #TODO also role.members
+    if message.content.lower().startswith("/add_task") or message.content.lower().startswith("/add") or message.content.lower().startswith("/a"):
         #if no there is no : then due date is set to a week
         # await message.channel.send(task + " added!")
         await message.channel.send(add_task(message.content.lower()))
         #just lists commands
-    elif message.content.lower().startswith("/show_tasks"):
+    elif message.content.lower().startswith("/show_tasks") or message.content.lower().startswith("/show") or message.content.lower().startswith("/s"):
         await message.channel.send(show_tasks(message.content.lower()))
-    elif message.content.lower().startswith("/finish_task"):
+    elif message.content.lower().startswith("/finish_task") or message.content.lower().startswith("/finish") or message.content.lower().startswith("/f"):
         await message.channel.send(finish_task(message.content.lower()))
+    elif message.content.lower().startswith("/edit_task") or message.content.lower().startswith("/edit") or message.content.lower().startswith("/e"):
+        await message.channel.send(edit_task(message.content.lower()))
     else:
         await message.channel.send("hmmm...try again")
 
