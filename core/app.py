cff65532b7ac0ce9bc93890a483f740f1e991dc3
r0nk <r0nk@simplecpu.com>
2022-02-14 14:25:03 -0600

    started .chalice api

---
+++ core/app.py
@@ -0,0 +1,34 @@
+from chalice import Chalice
+
+app = Chalice(app_name='core')
+
+
+@app.route('/')
+def index():
+    print("hello called");
+    return {'hello': 'world'}
+
+@app.route('/tasks/{name}')
+def task(name):
+    return {'name':name}
+
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
